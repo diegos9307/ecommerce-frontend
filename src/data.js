5d07935854278c13179cf195ff658a2beb3e4fa4
diegos9307 <diegos9307@gmail.com>
2022-08-18 23:59:43 -0500

    nav-bar and slider done

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://img.freepik.com/foto-gratis/retrato-joven-feliz-bolsas-compras_171337-2608.jpg?w=740&t=st=1660884659~exp=1660885259~hmac=dcdd02f261f8114e95104230ed4c732daf88b6cc6944f1160e2cef4b5628f970",
+    title: "SUMMER SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "f5fafd"
+  },
+  {
+    id: 2,
+    img: "https://i.ibb.co/DG69bQ4/2.png",
+    title: "AUTUMN COLLECTION",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fcf1ed"
+  },
+  {
+    id: 3,
+    img: "https://i.ibb.co/cXFnLLV/3.png",
+    title: "LOUNGEWEAR LOVE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fbf0f4"
+  }
+];
