10b1b714f7396055430363f59ed80f6377f03b2e
diegos9307 <diegos9307@gmail.com>
2022-08-24 22:27:49 -0500

    create components categories-products-newsletter-footer home is done

---
+++ src/components/Footer.js
@@ -0,0 +1,135 @@
+import styled from "styled-components";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import PinterestIcon from "@mui/icons-material/Pinterest";
+import HomeIcon from "@mui/icons-material/Home";
+import PhoneIcon from "@mui/icons-material/Phone";
+import EmailIcon from "@mui/icons-material/Email";
+
+const Container = styled.div`
+  display: flex;
+  background-color: rgb(0, 128, 128, 0.15);
+`;
+const Left = styled.div`
+  flex: 1mm;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+`;
+
+const Logo = styled.h2``;
+
+const Desc = styled.p`
+  margin: 10px 0px 20px;
+`;
+
+const SocialContainer = styled.div`
+  display: flex;
+`;
+
+const SocialIcon = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  color: white;
+  background-color: #${(props) => props.color};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-right: 20px;
+`;
+
+const Center = styled.div`
+  flex: 1;
+  padding: 20px;
+`;
+
+const Title = styled.h3`
+  margin-bottom: 30px;
+`;
+
+const List = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const ListItem = styled.li`
+  width: 50%;
+  margin-bottom: 10px;
+`;
+
+const Right = styled.div`
+  flex: 1;
+  padding: 20px;
+`;
+
+const ContactItem = styled.div`
+  margin-bottom: 20px;
+  display: flex;
+  align-items: center;
+`;
+
+const Payment = styled.img`
+  width: 50%;
+`;
+
+const Footer = () => {
+  return (
+    <Container>
+      <Left>
+        <Logo>DAS.</Logo>
+        <Desc>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio, cum! Saepe at nisi qui,
+          adipisci magni explicabo incidunt voluptatum harum illo nulla, velit illum dicta maxime
+          quas, ducimus reiciendis quibusdam?
+        </Desc>
+        <SocialContainer>
+          <SocialIcon color={"3B5999"}>
+            <FacebookIcon />
+          </SocialIcon>
+          <SocialIcon color={"E4405F"}>
+            <InstagramIcon />
+          </SocialIcon>
+          <SocialIcon color={"E60023"}>
+            <PinterestIcon />
+          </SocialIcon>
+        </SocialContainer>
+      </Left>
+      <Center>
+        <Title>Useful Links</Title>
+        <List>
+          <ListItem>Home</ListItem>
+          <ListItem>Cart</ListItem>
+          <ListItem>Man Fashion</ListItem>
+          <ListItem>Woman Fashion</ListItem>
+          <ListItem>Accessories</ListItem>
+          <ListItem>My Account</ListItem>
+          <ListItem>Order Tracking</ListItem>
+          <ListItem>Wishlist</ListItem>
+          <ListItem>Cart</ListItem>
+          <ListItem>Terms</ListItem>
+        </List>
+      </Center>
+      <Right>
+        <Title>Contact</Title>
+        <ContactItem>
+          <HomeIcon style={{ marginRight: "10px" }} /> CRA 43A #71 sur - 103
+        </ContactItem>
+        <ContactItem>
+          <PhoneIcon style={{ marginRight: "10px" }} />
+          +57 3204932599
+        </ContactItem>
+        <ContactItem>
+          <EmailIcon style={{ marginRight: "10px" }} />
+          diegos9307@gmail.com
+        </ContactItem>
+        <Payment src={"https://i.ibb.co/Qfvn4z6/payment.png"} />
+      </Right>
+    </Container>
+  );
+};
+
+export default Footer;
